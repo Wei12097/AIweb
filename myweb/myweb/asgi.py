e3a31cb2bca3576a2b68f23cd24d8f717f1ba7dd
Wei <1209775934@qq.com>
2023-11-28 15:56:46 +0800

    first version

--- myweb/myweb/asgi.py
+++ myweb/myweb/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ocrweb project.
+ASGI config for myweb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ocrweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myweb.settings')
 
 application = get_asgi_application()
--- myweb/myweb/urls.py
+++ myweb/myweb/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ocrweb project.
+URL configuration for myweb project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
